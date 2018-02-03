e8567120a19f525509f077a1de9b35ac30878c06
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2018-02-03 18:48:50 +0100

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -2,27 +2,22 @@
 // attatch this script to a player game object that has a rigidbody component attached
 // the ground objects in the scene need to have a "Platform" tag
 
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class PlayerController : MonoBehaviour 
 {
-
     public float speed;
     public float jumpforce;
     private Rigidbody rbody;
     private bool jumping;
     private bool isgrounded;
 
-
     private void Start()
     {
         rbody = GetComponent<Rigidbody>();
     }
-
 
     private void Update()
     {
@@ -32,29 +27,24 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-
     private void FixedUpdate()
     {
         // move
         float h = Input.GetAxis("Horizontal");
         rbody.velocity = new Vector3(h * speed, rbody.velocity.y, 0.0f);
-
         // jump
         if (jumping)
         {
             rbody.AddForce(0.0f, jumpforce, 0.0f, ForceMode.Impulse);
             jumping = false;
             isgrounded = false;
-
         }
     }
-
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Platform")
             isgrounded = true;
     }
-
 }
  
