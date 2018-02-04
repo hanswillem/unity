2b1da866e35eed8bcece127568ea956afb0f975f
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2018-02-04 10:55:59 +0100

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -6,7 +6,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour 
+public class PlayerController : MonoBehaviour
 {
     public float speed;
     public float jumpforce;
